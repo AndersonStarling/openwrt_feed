69695e5549f95a8f4cfb61a4cccc7f2665aaaebd
tai.nguyen <tai.nguyen24816@gmail.com>
2026-06-08 23:25:28 +0700

    add example repawn service

---
+++ custom_package/app_monitor/src/app_monitor.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int main()
+{
+    while(1)
+    {
+        printf("Hello, I am alive!\n");
+        sleep(3);
+    }
+
+    return 0;
+}
